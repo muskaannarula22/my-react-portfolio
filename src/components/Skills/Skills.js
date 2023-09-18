a72a0780c5d8ecc21dd287f0c248eb69eb6201dd
muskaannarula22 <muskaannarula22@vt.edu>
2023-09-18 06:28:13 +0000

    changed color of skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -12,8 +12,8 @@ function Skills() {
     const { theme } = useContext(ThemeContext);
 
     const skillBoxStyle = {
-        backgroundColor: theme.secondary,
-        boxShadow: `0px 0px 30px ${theme.primary30}`
+        backgroundColor: theme.tertiarySkills,
+        boxShadow: `0px 0px 30px ${theme.tertiarySkillsHover}`
     }
 
     return (
@@ -25,8 +25,8 @@ function Skills() {
 
                         {skillsData.map((skill, id) => (
                             <div className="skill--box" key={id} style={skillBoxStyle}>
-                                <img src={skillsImage(skill)} alt={skill} />
-                                <h3 style={{color: theme.tertiary}}>
+                                <img src={skillsImage(skill)} alt={skill} backgroundColor={theme.secondary}/>
+                                <h3 style={{color: theme.secondary}}>
                                     {skill}
                                 </h3>
                             </div>
